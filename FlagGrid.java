633b51a731224700cfb855b7e89f2fbbc5946fd4
CristianNumberThree <152836270+CristianNumberThree@users.noreply.github.com>
2024-09-22 15:24:00 -0400

    Update FlagGrid.java

--- FlagGrid.java
+++ FlagGrid.java
@@ -1,38 +1,54 @@
-import java.awt.*;
-import javax.swing.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-public class FlagGrid extends JFrame {
+public class FlagGrid extends Application {
 
-    public FlagGrid() {
-        // Set the title of the window
-        setTitle("Flag Grid");
-
-        // Set the layout to a 2x2 grid
-        setLayout(new GridLayout(2, 2));
+    @Override
+    public void start(Stage primaryStage) {
+        // Create a GridPane layout
+        GridPane gridPane = new GridPane();
 
         // Load and add the images to the grid
-        add(new JLabel(new ImageIcon("flag1.gif")));
-        add(new JLabel(new ImageIcon("flag2.gif")));
-        ImageIcon flag6Icon = new ImageIcon("flag6.gif");
-        Image scaledFlag6 = flag6Icon.getImage().getScaledInstance(412, 217, Image.SCALE_SMOOTH);
-        add(new JLabel(new ImageIcon(scaledFlag6)));
-        ImageIcon flag7Icon = new ImageIcon("flag7.gif");
-        Image scaledFlag7 = flag7Icon.getImage().getScaledInstance(417, 217, Image.SCALE_SMOOTH);
-        add(new JLabel(new ImageIcon(scaledFlag7)));
+        ImageView flag1 = new ImageView(new Image("flag1.gif"));
+        ImageView flag2 = new ImageView(new Image("flag2.gif"));
+        
+        // Load flag6 and scale it
+        Image flag6Image = new Image("flag6.gif");
+        ImageView flag6 = new ImageView(flag6Image);
+        flag6.setFitWidth(412);  // Set width
+        flag6.setFitHeight(217); // Set height
+        flag6.setPreserveRatio(true);
 
-        // Set default close operation
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Load flag7 and scale it
+        Image flag7Image = new Image("flag7.gif");
+        ImageView flag7 = new ImageView(flag7Image);
+        flag7.setFitWidth(417);  // Set width
+        flag7.setFitHeight(217); // Set height
+        flag7.setPreserveRatio(true);
 
-        // Set the size of the window
-        setSize(855, 480);
+        // Add the images to the grid
+        gridPane.add(flag1, 0, 0);  // First image in row 0, column 0
+        gridPane.add(flag2, 1, 0);  // Second image in row 0, column 1
+        gridPane.add(flag6, 0, 1);  // Third image in row 1, column 0
+        gridPane.add(flag7, 1, 1);  // Fourth image in row 1, column 1
 
-        // Make the window visible
-        setVisible(true);
+        // Create a Scene and set it on the primary stage
+        Scene scene = new Scene(gridPane, 855, 480);  // Scene size matches the window size
+        primaryStage.setScene(scene);
+
+        // Set the title of the window
+        primaryStage.setTitle("Flag Grid");
+
+        // Show the stage (window)
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        // Invoke the constructor to create the GUI
-        new FlagGrid();
+        // Launch the JavaFX application
+        launch(args);
     }
 }
-
